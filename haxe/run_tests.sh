92bf50efbeab4c000223df16a0297db754d2757e
Georg Koester <mail@georgkoester.com>
2015-01-10 18:28:07 +0100

    Fix argument handling and exit code display in run_tests.sh.

--- haxe/run_tests.sh
+++ haxe/run_tests.sh
@@ -5,7 +5,7 @@ if [ -z ${1+x} ]; then
   #run all
   echo "no argument given, running all";
 else
-  TO_TEST="$1";
+  TO_TEST="$@";
 fi
 
 RESULTS="";
@@ -13,7 +13,7 @@ EXIT_STATUS=0;
 
 echo "running $TO_TEST";
 cd haxe_repo/tests
-for i in $TO_TEST; do
+for i in $TO_TEST; do # no quotes around to_test here - only single words allowed
   export TEST=$i;
   if [ "$i" = "php" ]; then
     /etc/init.d/mysql start;
@@ -23,7 +23,7 @@ for i in $TO_TEST; do
   if [ $THIS_RUN_STATUS != 0 ]; then
     EXIT_STATUS=1;
   fi
-  RESULTS="$RESULTS \n$TEST exit status: $?";
+  RESULTS="$RESULTS \n$TEST exit status: $THIS_RUN_STATUS";
 done
 
 echo -e "\n\n Results: $RESULTS";
